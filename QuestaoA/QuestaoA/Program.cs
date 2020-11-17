bcfde8d14fc365c413b0e4e88a2762909679e766
Carlos Guilherme Miranda Finelli <60704748+CarlosFinelli@users.noreply.github.com>
2020-11-17 17:14:35 -0300

    Conclusão questão A
    Conclusão da Questão A utilizando de propriedades para o get e set

---
+++ QuestaoA/QuestaoA/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace QuestaoA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            inicio:
+            Console.Write("Insira a Largura do Retângulo: ");
+            retangulo.Largura = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira a Altura do Retângulo: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            retangulo.Result = retangulo.Altura * retangulo.Largura;
+            Console.WriteLine($"Área do retângulo: {retangulo.Result}");
+            Console.WriteLine();
+            retangulo.Result = 2 * (retangulo.Altura + retangulo.Largura);
+            Console.WriteLine($"Perímetro do retângulo: {retangulo.Result}");
+            Console.WriteLine();
+            retangulo.Result = Math.Sqrt(Math.Pow(retangulo.Altura, 2) + Math.Pow(retangulo.Largura, 2));
+            Console.WriteLine($"Diagonal do retângulo: {retangulo.Result}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadKey();
+            Console.Write("Deseja realizar outro cálculo? (1 = Sim, 2 = Não): ");
+            int opcao = Convert.ToInt16(Console.ReadLine());
+            if (opcao == 1)
+            {
+                goto inicio;
+            } else
+            {
+                Console.Clear();
+                Console.WriteLine("Obrigado por utilizar o programa.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ QuestaoA/QuestaoA/Retangulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuestaoA
+{
+    class Retangulo
+    {
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+        public double Result { get; set; }
+    }
+}
